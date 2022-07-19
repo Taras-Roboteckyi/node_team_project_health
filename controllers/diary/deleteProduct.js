3cd626d189c124611ef5abb6ddc60bd5f2a69282
Taras Garan'ko <garanko.t@gmail.com>
2022-07-20 01:18:44 +0300

    fixdev

--- controllers/diary/deleteProduct.js
+++ controllers/diary/deleteProduct.js
@@ -1,21 +1,20 @@
-const {Diary}=require('../../models/diary');
-const {createError}=require('../../helpers/errors');
-const {deleteProduct}=require('../../services/diary/deleteProductService');
+// const {Diary}=require('../../models/diary');
+// const {createError}=require('../../helpers/errors');
+const { deleteProduct } = require("../../services/diary/deleteProductService");
 
-const deleteProductCtrl = async(req, res, next)=>{
-    try{
-        await deleteProduct(req.user._id, req.body);
-        return res.json({
-            status:"success",
-            code:200,
-            message:'product deleted'
-        });
-    }catch (err){
-        next(err)
-    }
+const deleteProductCtrl = async (req, res, next) => {
+  try {
+    await deleteProduct(req.user._id, req.body);
+    return res.json({
+      status: "success",
+      code: 200,
+      message: "product deleted",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
-}
-
-module.exports={
-    deleteProductCtrl
-}
+module.exports = {
+  deleteProductCtrl,
+};
