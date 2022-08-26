4b6c04478be4afb780e800c47d9e84c680100878
valentynadymova <valentynadymova@gmail.com>
2022-08-26 11:48:37 +0200

    new diary model []

--- controllers/diary/addProduct.js
+++ controllers/diary/addProduct.js
@@ -1,23 +1,42 @@
-// const {Diary}=require('../../models/diary');
-const {addProduct}=require('../../services/diary/addProductService');
+const {findProductByDateUser, findProductById}= require("../../services/diary")
+const {createNewProduct}=require('../../helpers/diary');
 
-
-
-const addProductCtrl = async(req,res,next)=>{
-
-    try{
-    const product = await addProduct(req.user._id, req.body);
-    const {_id, date, user, productWeight, productTitle, productCalories}=product;
-
-
-    return res.status(201).json({
-        data:{_id, date, user, productWeight, productTitle, productCalories}
-    })
-}catch (err){
-    next(err)
-}
-
-};
-
-
-module.exports={addProductCtrl}
+ 
+const addProduct = async (req, res) => {
+    const { _id: userId } = req.user;
+    const { date } = req.query;
+    const { id, weight } = req.body;
+  
+    const product = await findProductById(id);
+    const { _id: productId, title, calories } = product;
+  
+    const newProduct = createNewProduct(productId, title, weight, calories);
+    const checkDate = await findProductByDateUser(date, userId);
+  
+    if (checkDate) {
+      const checkedProduct = checkDate.consumedProducts.find(
+        (product) => product._id === id
+      );
+  
+      if (!checkedProduct) {
+        checkDate.consumedProducts.push(newProduct);
+        checkDate.total += newProduct.kcal;
+      } else {
+        checkedProduct.weight += newProduct.weight;
+        checkedProduct.kcal += Math.round(calories * (weight / 100));
+        checkDate.total += newProduct.kcal;
+      }
+  
+      await checkDate.save();
+    }
+  
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: {
+        ...newProduct,
+      },
+    });
+  };
+  
+  module.exports = {addProduct};
